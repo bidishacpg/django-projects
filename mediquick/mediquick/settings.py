1196ddaea2f7d2923cf0579a5010e6867dba17bc
bidishacpg <136471047+bidishacpg@users.noreply.github.com>
2024-05-14 11:01:34 +0545

    a

--- mediquick/mediquick/settings.py
+++ mediquick/mediquick/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'mediquick.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "mydata",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": 3306,
     }
 }
 
--- mediquick/news/models.py
+++ mediquick/news/models.py
@@ -6,7 +6,7 @@ class News(models.Model):
     news_title=models.CharField(max_length=50)
     news_desc=HTMLField()
     news_images=models.FileField(upload_to="news/",max_length=250,null=True,default=None)
-    news_slug=AutoSlugField(populate_from='news_title', unique=True,null=True,default=None)
+    #news_slug=AutoSlugField(populate_from='news_title', unique=True,null=True,default=None)
     
 # Create your models here.
 
